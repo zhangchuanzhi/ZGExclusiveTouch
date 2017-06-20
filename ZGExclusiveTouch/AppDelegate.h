64af1e5d22d5229c1ea795c8a747d56d6caf9be4
zhangzhi0903 <18310127735@163.com>
2017-06-20 09:29:38 +0800

    Initial Commit

---
+++ ZGExclusiveTouch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZGExclusiveTouch
+//
+//  Created by offcn_zcz32036 on 2017/6/20.
+//  Copyright © 2017年 offcn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
